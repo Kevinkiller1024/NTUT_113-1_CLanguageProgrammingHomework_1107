fb51e19634bc6deb74c7e4188975412ec5e6c364
Kevinkiller1024 <Kevinkiller9487@gmail.com>
2024-11-06 20:58:45 +0800

    2

--- 5.28/5.28/Main.cpp
+++ 5.28/5.28/Main.cpp
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 
 char a;
+int change(int q);
 
 int main() {
 	printf("請輸入一個英文字母以轉換大小寫：");
 	scanf_s("%c", &a);
-	if (a > 0x40 && a < 0x5B) {
-		printf("輸入字為大寫%c\n小寫為%c\n", a, a + 0x20);
-	}
-	else if(a < 0x7B && a > 0x60){
-		printf("輸入字為小寫%c\n大寫為%c\n", a, a - 0x20);
-	}
-	else{
-		printf("輸入錯誤\n");
-	}
+	change(a);
 	system("pause");
 	return 0;
+}
+
+int change(int q) {
+	if (q > 0x40 && q < 0x5B) {
+		printf("輸入字為大寫%c\n小寫為%c\n", q, q + 0x20);
+	}
+	else if (q < 0x7B && q > 0x60) {
+		printf("輸入字為小寫%c\n大寫為%c\n", q, q - 0x20);
+	}
+	else {
+		printf("輸入錯誤\n");
+	}
+	return 1;
 }
